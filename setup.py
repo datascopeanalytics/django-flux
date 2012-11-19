a8976fed1c78f790b220662b8a63c855d1d484fa
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2012-11-19 14:17:20 -0600

    added preliminary setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "django-flux",
+    version = "0.1",
+    description = "locally fetch, store, and present social media flux",
+    author = "Dean Malmgren",
+    author_email = "dean.malmgren@datascopeanalytics.com",
+    url = "http://github.com/deanmalmgren/django-flux",
+    packages = ['flux'],
+)
