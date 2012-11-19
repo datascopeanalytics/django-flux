3491d3f78e974d84f58de4ac467e9109a204b8a0
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2012-11-19 16:34:29 -0600

    changed models to putting everyting in the same db

--- flux/models.py
+++ flux/models.py
@@ -1,90 +1,55 @@
 from django.db import models
 
-class BaseAccount(models.Model):
-    """Base model for storing information about each account we want
-    to track.
+class Account(models.Model):
+    """Model for storing information about each account we want to
+    track.
     """
-    
-    class Meta:
-        abstract = True
 
-    id = models.CharField(
+    class Meta:
+        unique_together = (("name", "type", ), )
+    
+    TYPES = (
+        ("twitter", "Twitter"),
+
+        # # FUTURE
+        # ("rss", "RSS"),
+        # ("facebook", "Facebook"),
+        # ("google+", "Google+"),
+        # ("linkedin", "LinkedIn"),
+    )
+
+    name = models.CharField(
         max_length=255,
-        unique=True,
-        primary_key=True,
-        help_text="Enter the id of the account to track",
+        db_index=True,
+        help_text="Enter the name of the account to track",
+    )
+    type = models.CharField(
+        max_length=16,
+        db_index=True,
+        choices=TYPES,
+        help_text="Select the type of account this is.",
     )
     other = models.TextField(
         help_text="Other data associated with this account. JSON format.",
     )
 
-class BaseStats(models.Model):
-    """Base model for storing social activity stats for various data feeds.
+class Flux(models.Model):
+    """Model for storing social activity flux for various data feeds.
     """
 
     class Meta:
-        abstract = True
+        unique_together = (("account", "date", ), )
 
-    # account = models.ForeignKey(
-    #     BaseAccount,
-    #     db_index=True,
-    #     help_text="THIS IS OVERRIDDEN BY ALL CHILD CLASSES",
-    # )
+    account = models.ForeignKey(
+        Account,
+        db_index=True,
+        help_text="Select the account with which this Flux datum is associated",
+    )
     date = models.DateField(
-        unique=True,
+        db_index=True,
         help_text="Enter the date the content was shared.",
     )
     count = models.IntegerField(
         default=0,
         help_text="Enter the number of pieces of content per day",
     )
-
-class TwitterAccount(BaseAccount):
-    """Twitter accounts to track
-    """
-
-class TwitterStats(BaseStats):
-    """Track the number of tweets that are posted per day by this
-    twitter account
-    """
-    account = models.ForeignKey(
-        TwitterAccount,
-        db_index=True,
-    )
-
-# class RssAccount(BaseAccount):
-#     """RSS feeds to track
-#     """
-
-# class RssStats(BaseStats):
-#     """Track the number of articles that are posted per day in the
-#     specified RSS feed.
-#     """
-#     account = models.ForeignKey(
-#         RssAccount,
-#         db_index=True,
-#     )
-
-# class LinkedInAccount(BaseAccount):
-#     """LinkedIn feeds to track
-#     """
-
-# class LinkedInStats(BaseStats):
-#     """Track the number of posts per day to the specified LinkedIn account.
-#     """
-#     account = models.ForeignKey(
-#         LinkedInAccount,
-#         db_index=True,
-#     )
-
-# class FacebookAccount(BaseAccount):
-#     """Facebook accounts to track
-#     """
-
-# class FacebookStats(BaseStats):
-#     """Track the number of posts per day to this Facebook account.
-#     """
-#     account = models.ForeignKey(
-#         FacebookAccount,
-#     )
-
