788e2c23dc747935154a8d8daff18ba69ecf5794
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2012-11-19 09:27:18 -0600

    started to generalize beyond storing a single account

--- flux/models.py
+++ flux/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 
-# Create your models here.
-<<<<<<< local
-=======
+class BaseAccount(models.Model):
+    """Base model for storing information about each account we want
+    to track.
+    """
+    
+    class Meta:
+        abstract = True
 
-class SocialActivityStats(models.Model):
+    id = models.CharField(
+        max_length=255,
+        unique=True,
+        primary_key=True,
+        help_text="Enter the id of the account to track",
+    )
+    other = models.TextField(
+        help_text="Other data associated with this account. JSON format.",
+    )
+
+class BaseStats(models.Model):
     """Base model for storing social activity stats for various data feeds.
     """
 
     class Meta:
         abstract = True
 
+    account = models.ForeignKey(
+        BaseAccount,
+        db_index=True,
+        help_text="THIS IS OVERRIDDEN BY ALL CHILD CLASSES",
+    )
     date = models.DateField(
         unique=True,
         help_text="Enter the date the content was shared.",
@@ -20,25 +39,49 @@ class SocialActivityStats(models.Model):
         help_text="Enter the number of pieces of content per day",
     )
 
-class TwitterStats(SocialActivityStats):
-    """Track the number of tweets that are posted per day by the
-    twitter account specified by SOCIAL_ACTIVITY_TWITTER_USERNAME
+class TwitterAccount(Account):
+    """Twitter accounts to track
     """
 
-class RssStats(SocialActivityStats):
-    """Track the number of articles that are posted per day in the RSS
-    feed specified by SOCIAL_ACTIVITY_RSS_URL
+class TwitterStats(BaseStats):
+    """Track the number of tweets that are posted per day by this
+    twitter account
     """
+    account = models.ForeignKey(
+        TwitterAccount,
+    )
 
-class FacebookStats(SocialActivityStats):
-    """Track the number of posts shared on Facebook that are posted
-    per day by the facebook page account specified by
-    SOCIAL_ACTIVITY.FACEBOOK.XXXX
-    """
+# class RssAccount(BaseAccount):
+#     """RSS feeds to track
+#     """
 
-class LinkedInStats(SocialActivityStats):
-    """Track the number of posts shared on LinkedIn that are posted
-    per day by the LinkedIn account specified by
-    SOCIAL_ACTIVITY.LINKEDIN.XXXX
-    """
->>>>>>> other
+# class RssStats(BaseStats):
+#     """Track the number of articles that are posted per day in the
+#     specified RSS feed.
+#     """
+#     account = models.ForeignKey(
+#         RssAccount,
+#     )
+
+# class LinkedInAccount(BaseAccount):
+#     """LinkedIn feeds to track
+#     """
+
+# class LinkedInStats(BaseStats):
+#     """Track the number of posts per day to the specified LinkedIn account.
+#     """
+#     account = models.ForeignKey(
+#         LinkedInAccount,
+#     )
+
+# class FacebookAccount(BaseAccount):
+#     """Facebook accounts to track
+#     """
+
+# class FacebookStats(BaseStats):
+#     """Track the number of posts per day to this Facebook account.
+#     """
+#     account = models.ForeignKey(
+#         FacebookAccount,
+#     )
+
