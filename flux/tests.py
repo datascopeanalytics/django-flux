6f993b622134486a9b55f731dc03e15404341994
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2012-11-21 09:28:28 -0600

    added test for bins going into the future

--- flux/tests.py
+++ flux/tests.py
@@ -1,16 +1,26 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import datetime
 
 from django.test import TestCase
 
+import models
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# class SimpleTest(TestCase):
+#     def test_basic_addition(self):
+#         """
+#         Tests that 1 + 1 always equals 2.
+#         """
+#         self.assertEqual(1 + 1, 2)
+
+class AccountTest(TestCase):
+    
+    fixtures = ['test_data.json', ]
+
+    def test_timeseries_bins(self):
+        """timeseries bins should not extend into future"""
+        account = models.Account.objects.get(pk=1)
+        timeseries = account.get_timeseries()
+        for bin in timeseries:
+            self.assertTrue(
+                bin.end<datetime.date.today(), 
+                "bin '%s' extends into the future!" % bin,
+            )
