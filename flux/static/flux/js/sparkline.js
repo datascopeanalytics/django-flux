f9ad63de2f1a437dce546e9724f0b88b13214ff1
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-02-05 07:05:49 -0600

    added time axis for the sparkline

--- flux/static/flux/js/sparkline.js
+++ flux/static/flux/js/sparkline.js
@@ -1,3 +1,8 @@
+// create a global variable to store the bottom margin of the svg so
+// that the sparkline_mouseover_labels.js script can see the same
+// boundary
+document.flux_bottom_margin = 10;
+
 $(document).ready(function () {
     
     // TODO: only run this functionality when the browser is svg
@@ -34,15 +39,21 @@ $(document).ready(function () {
 
     	    // map the index of the element in the data array to the
     	    // width of the svg
+	    var w = $(this).width();
     	    var x = d3.scale.linear()
     		.domain([0, data[k].length])
-    		.range([0, $(this).width()]);
+    		.range([0, w]);
+	    var t = d3.time.scale()
+    		.domain([data[k][0].beg, data[k][data[k].length-1].end])
+    		.range([0, w]);
 
     	    // map the value of the element in the data array to the
     	    // height of the svg
+	    var h = $(this).height();
+	    var margin = document.flux_bottom_margin;
     	    var y = d3.scale.linear()
     		.domain([d3.max(data[k].map(function (d) {return d.count})), 0])
-    		.range([0, $(this).height()]);
+    		.range([0, h-margin]);
 
     	    // create a line path from the data
     	    var line = d3.svg.line().x(function (d, i) {
@@ -55,6 +66,26 @@ $(document).ready(function () {
     	    d3.select(this)
     		.append("svg:path")
     		.attr("d", line(data[k]));
+
+	    // add the time axis to the DOM. useful links:
+	    // http://jsfiddle.net/robdodson/KWRxW/
+	    // https://github.com/mbostock/d3/wiki/Time-Formatting
+	    var format = d3.time.format('%b');
+	    function tick_format (d, i) {
+		return format(d, i).substr(0,1)
+	    }
+	    tAxis = d3.svg.axis()
+		.scale(t)
+		.orient('bottom')
+		.ticks(d3.time.months, 1)
+		.tickFormat(tick_format)
+		.tickSize(0)
+		.tickPadding(3);
+	    d3.select(this)
+		.append('g')
+		.attr('class', 't axis')
+		.attr('transform', 'translate(0, ' + (h - margin)+')')
+		.call(tAxis);
 
     });
 
